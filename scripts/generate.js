f95a23a0456362181a08550351975e2a62d3a969
Wentz Chen <me@pannnda.com>
2019-11-18 22:06:35 +0800

    chore: 构建基础的项目目录

---
+++ scripts/generate.js
@@ -0,0 +1,22 @@
+const fs = require('fs')
+const path = require('path')
+const matter = require('gray-matter')
+
+const articlesDir = path.join(__dirname, '../articles')
+const articles = fs
+  .readdirSync(articlesDir)
+  .filter(filename => filename.indexOf('.') !== 0 && filename.split('.').splice(-1)[0] === 'md')
+const parseArticles = articles.map(article =>
+  matter(fs.readFileSync(path.join(articlesDir, article)))
+)
+console.log(parseArticles)
+
+fs.writeFileSync(
+  path.join(__dirname, '../public/__blogz__.js'),
+  `window.__BLOGZ__ = ${JSON.stringify(parseArticles)}`
+)
+
+// fs.writeFileSync(
+//   path.join(__dirname, '../src/assets/_gen/__blogz__.json'),
+//   JSON.stringify(parseArticles)
+// )
