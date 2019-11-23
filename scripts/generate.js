647744842b53aff708d1022dac7d3b7fcf261ea0
Wentz Chen <me@pannnda.com>
2019-11-23 19:45:27 +0800

    feat: 添加 build 时将 article 文件移动到静态目录下

--- scripts/generate.js
+++ scripts/generate.js
@@ -6,10 +6,9 @@ const moment = require('moment')
 const __BLOGZ__ = {
   blogs: []
 }
-
-// 解析 Markdown
 const articlesDir = path.join(__dirname, '../articles')
-const articles = fs
+const _articlesDir = path.join(__dirname, '../public/_articles')
+const articleFileNames = fs
   .readdirSync(articlesDir)
   .filter(
     filename =>
@@ -17,20 +16,33 @@ const articles = fs
       filename.split('.').splice(-1)[0] === 'md' &&
       filename !== 'README.md'
   )
-const parseArticles = articles.map(article => ({
-  ...matter(fs.readFileSync(path.join(articlesDir, article))),
-  enTitle: article.split('.')[0]
+const articlePaths = articleFileNames.map(filename => path.join(articlesDir, filename))
+
+// 解析 Markdown
+const parseArticles = articleFileNames.map(filename => ({
+  ...matter(fs.readFileSync(path.join(articlesDir, filename))),
+  en_title: filename.split('.')[0],
+  url: `/_articles/${filename}`
 }))
 
 // 按时间倒叙排序
 parseArticles.sort((a, b) => (moment(a.data.date).isBefore(b.data.date) ? 1 : -1))
-//console.log(parseArticles)
 
 // 所有博客摘要
-__BLOGZ__.blogs = parseArticles.map(s => ({ ...s.data, enTitle: s.enTitle }))
-console.log(__BLOGZ__.blogs)
+__BLOGZ__.blogs = parseArticles.map(s => ({ ...s.data, en_title: s.en_title, url: s.url }))
 
+// 将 __BLOGZ__ 写入 /public/__blogz__.js
 fs.writeFileSync(
   path.join(__dirname, '../public/__blogz__.js'),
   `window.__BLOGZ__ = ${JSON.stringify(__BLOGZ__)}`
 )
+
+// 将 articles 文件夹中的文章移动到 public/_articles 下
+articleFileNames.map(filename => {
+  const articlePath = path.join(articlesDir, filename)
+  const _articlePath = path.join(_articlesDir, filename)
+  if (!fs.existsSync(_articlesDir)) {
+    fs.mkdirSync(_articlesDir)
+  }
+  fs.writeFileSync(_articlePath, fs.readFileSync(articlePath))
+})
