267c3228c0e4222d54625b852a45ff902296c6b1
Wentz Chen <me@pannnda.com>
2019-11-23 13:14:32 +0800

    fix(Header): 修复 Header box-shadow 判断

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -14,7 +14,7 @@ const Header: React.FC = () => {
   const [showMenu, setShowMenu] = useState(false)
   const isAdapt = useAdapt()
   return (
-    <header style={{ background: isAdapt ? '#fff' : 'none' }}>
+    <header className={classNames({'header-adapt': isAdapt})}>
       <div className="header-inner flex jc-sb ai-center">
         <Link to="/" style={{ height: 'auto' }}>
           <div className={classNames('brand', { 'brand-adapt': isAdapt })}>Wentz's Blog</div>
