761f34e255d802e102ca26ded69ab2dac39dfb83
Wentz Chen <me@pannnda.com>
2019-11-23 16:58:59 +0800

    feat: 完成文章摘要的集合和排序

--- scripts/generate.js
+++ scripts/generate.js
@@ -1,7 +1,13 @@
 const fs = require('fs')
 const path = require('path')
 const matter = require('gray-matter')
+const moment = require('moment')
 
+const __BLOGZ__ = {
+  blogs: []
+}
+
+// 解析 Markdown
 const articlesDir = path.join(__dirname, '../articles')
 const articles = fs
   .readdirSync(articlesDir)
@@ -11,12 +17,20 @@ const articles = fs
       filename.split('.').splice(-1)[0] === 'md' &&
       filename !== 'README.md'
   )
-const parseArticles = articles.map(article =>
-  matter(fs.readFileSync(path.join(articlesDir, article)))
-)
-console.log(parseArticles)
+const parseArticles = articles.map(article => ({
+  ...matter(fs.readFileSync(path.join(articlesDir, article))),
+  enTitle: article.split('.')[0]
+}))
+
+// 按时间倒叙排序
+parseArticles.sort((a, b) => (moment(a.data.date).isBefore(b.data.date) ? 1 : -1))
+//console.log(parseArticles)
+
+// 所有博客摘要
+__BLOGZ__.blogs = parseArticles.map(s => ({ ...s.data, enTitle: s.enTitle }))
+console.log(__BLOGZ__.blogs)
 
 fs.writeFileSync(
   path.join(__dirname, '../public/__blogz__.js'),
-  `window.__BLOGZ__ = ${JSON.stringify(parseArticles)}`
+  `window.__BLOGZ__ = ${JSON.stringify(__BLOGZ__)}`
 )
