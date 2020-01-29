047fbd86989ea1fefd51c041f889f8bb675a61ca
Wentz Chen <me@pannnda.com>
2020-01-29 20:40:12 +0800

    feat: 完成 content sidebar 布局

---
+++ src/components/Sidebar/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './index.less'
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="me">me</div>
+    </div>
+  )
+}
+
+export default Sidebar
--- src/views/Index/index.tsx
+++ src/views/Index/index.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react'
 import Banner from '@/components/Banner'
+import Sidebar from '@/components/Sidebar'
 import './index.less'
 
 const Index: React.FC = () => {
   return (
     <div className="content">
       <Banner title="Wentz's Blog" image={require('@/assets/images/index-background.jpg')} />
+      <div className="content-container flex jc-sb">
+        <div className="article-list"></div>
+        <Sidebar />
+      </div>
     </div>
   )
 }
